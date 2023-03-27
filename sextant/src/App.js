367c2080fc43f0f3ddd5bdc98b627f009df71774
zhlavenh <zeehlavenhouse@gmail.com>
2023-03-27 15:47:08 -0400

    Worked on components and add style holder file

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -13,31 +13,18 @@ export function Banner(){
   return(
     <div className="Banner">
       <h1>Sextant</h1>
-      <button type="button" className="banner-collapse" onClick={clickCollaspse}>
-        <span className="material-icons banner-opened">
-          keyboard_arrow_down
-        </span>
-      </button>
     </div>
   );
 }
 
-export function Exhibit(){
+export function Exhibit(props){
   return(
-    <div className="Exhibit">
-      {/* Location of card components */}
-    </div>
-  );
-}
-
-function cardContainer(props){
-  return(
-    <div className="card">
-      <h2 className="cardHeader">{props.header}</h2>
-      <div className="cardInfo">
-        <p className="userIP">{props.userIP}</p>
-        <p className="plyonInfo">{props.pylonInfo}</p>
+    <div className="exhibit">
+      <h2 className="exhibitHeadng">{props.heading}</h2>
+      <div className="exhibitInfo">
+        {props.info}
       </div>
     </div>
   );
 }
+
--- sextant/src/index.js
+++ sextant/src/index.js
@@ -7,11 +7,13 @@ import {Banner, Exhibit} from './App';
 
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Banner />
-    <Exhibit />
+    <Exhibit heading="Test1" info="Test Infomation"/>
+    <Exhibit heading="Test2" info="Test Infomation"/>
     <App />
   </React.StrictMode>
 );
